ab41ee637692ffdfc8f0f0cf62af0dc2717a40fb
abdelkarim <skwyz01@outlook.com>
2023-01-29 18:40:52 +0100

    add : can parse simple expressions

--- src/CPT/Cpt.hs
+++ src/CPT/Cpt.hs
@@ -1,7 +1,32 @@
-module CPT.Cpt where
+import Data.Char (isDigit, isSpace)
 
-data Cpt = Integer Int 
-        |   Symbols String
-        |   Lists [Cpt]
+data Cpt = Lists [Cpt] | Symbols String | Integer Int deriving (Eq, Show)
 
+parseTree :: String -> Maybe (Cpt, String)
+parseTree s = case s of
+    "" -> Nothing
+    ('(':xs) -> do
+        (cpts, rest) <- parseList xs
+        return (Lists cpts, rest)
+    (x:xs) | isDigit x -> do
+        (num, rest) <- parseInteger (x:xs)
+        return (Integer num, rest)
+    (x:xs) -> do
+        let (symbol, rest) = parseSymbol (x:xs)
+        return (Symbols symbol, rest)
 
+parseList :: String -> Maybe ([Cpt], String)
+parseList s = case s of
+    (')':xs) -> Just ([], xs)
+    _ -> do
+        (cpt, rest) <- parseTree s
+        (cpts, rest') <- parseList rest
+        return (cpt:cpts, rest')
+
+parseSymbol :: String -> (String, String)
+parseSymbol s = let (symbol, rest) = span (not . isSpace) s in (symbol, dropWhile isSpace rest)
+
+parseInteger :: String -> Maybe (Int, String)
+parseInteger s = case reads s of
+    [(n, rest)] -> Just (n, rest)
+    _ -> Nothing
