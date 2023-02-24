24c6711ccd2faac6356ed0bbbc5d75c4c7b7d3d2
Vincent Zhong <vincent.zhong@epitech.eu>
2023-02-24 20:11:17 +0100

    add: compilation and simple tests with it

--- src/Compile.hs
+++ src/Compile.hs
@@ -5,23 +5,35 @@ module Compile
 
 import Lib
 
--- astListsToInstructions :: [Ast] -> Int -> ((Either [Instruction] String), Int)
--- astListsToInstructions [] i = (Left [], i)
--- astListsToInstructions (a:b) i = case astToInstructions a i of
-    -- (Right err, _) -> (Right err, i)
-    -- (Left il, index) -> case astListsToInstructions b (index + 1) of
-        -- (Right err, _) -> (Right err, index)
-        -- (Left res, ind) -> (Left (il ++ res), ind + 1)
+addInstruction :: (Either [Instruction] String) -> Instruction -> ((Either [Instruction] String), Int)
+addInstruction a ni = case a of
+    (Right err) -> (Right err, line ni)
+    (Left li) -> (Left (li ++ [ni]), line ni + 1)
+
+astListToInstructions :: [Ast] -> Int -> ((Either [Instruction] String), Int)
+astListToInstructions [] i = (Left [], i)
+astListToInstructions (a:b) i = case astToInstructions a i of
+    (Right err, _) -> (Right err, i)
+    (Left il, index) -> case astListToInstructions b index of
+        (Right err, _) -> (Right err, index)
+        (Left res, ind) -> (Left (il ++ res), ind)
 
 -- Return :
 --  List of instruction OR error
 --  Index of the last line + 1
+callToInstructions :: Ast -> Int -> ((Either [Instruction] String), Int)
+callToInstructions (AstCall []) i = (Right "? invalid call", i)
+callToInstructions (AstCall (_:b)) i = astListToInstructions b i
+callToInstructions _ i = (Right "? invalid call", i)
 
--- define
--- lambda
--- call
 astToInstructions :: Ast -> Int -> ((Either [Instruction] String), Int)
+astToInstructions (AstDefine a v) i = case astToInstructions v i of
+    (Right err, ni) -> (Right err, ni)
+    (li, ni) -> addInstruction li Instruction {line = ni, command = "call", value = Just (AstDefine a v)}
 astToInstructions (AstCall (AstSymbol "if":b)) i = ifToInstructions 0 (AstCall (AstSymbol "if":b)) i
+astToInstructions (AstCall (a:b)) i = case callToInstructions (AstCall (a:b)) i of
+    (Right err, ni) -> (Right err, ni)
+    (li, ni) -> addInstruction li Instruction {line = ni, command = "call", value = Just a}
 astToInstructions (AstSymbol a) i = (Left [Instruction {line = i, command = "get", value = Just (AstSymbol a)}], i + 1)
 astToInstructions a i = (Left [Instruction {line = i, command = "push", value = Just a}], i + 1)
 
@@ -29,16 +41,17 @@ ifToInstructions :: Int -> Ast -> Int -> ((Either [Instruction] String), Int)
 ifToInstructions r (AstCall (_:cond:yes:no:[])) i = case r of
     -- return
     1 -> (ifList [
-        lcond, (Left [Instruction {line = ic, command = "jumpIfFalse", value = Just (AstInteger (iy + 1))}]),
+        lcond, (Left [Instruction {line = ic, command = "jumpIfFalse", value = Just (AstInteger (iy + r))}]),
         lyes, Left [Instruction {line = iy, command = "return", value = Nothing}],
         lno, Left [Instruction {line = ino, command = "return", value = Nothing}]
         ], ino + 1)
     -- no return
     0 -> (ifList [
-        lcond, (Left [Instruction {line = ic, command = "jumpIfFalse", value = Just (AstInteger (iy + 1))}]),
+        lcond, (Left [Instruction {line = ic, command = "jumpIfFalse", value = Just (AstInteger (iy + r))}]),
         lyes,
         lno
-        ], ino + 1)
+        ], ino)
+    _ -> (Right "if invalid call", i)
     where
         (lcond, ic) = astToInstructions cond i -- cond 
         (lyes, iy) = astToInstructions yes (ic + 1) -- cond + jump + true
@@ -50,17 +63,18 @@ ifToInstructions r (AstCall (_:cond:yes:no:[])) i = case r of
                 (Left ret) -> Left (il ++ ret)
                 Right err -> Right err
             Right err -> Right err
+ifToInstructions _ _ i = (Right "if invalid call", i)
 
 compileExpression :: Ast -> Int -> ((Either [Instruction] String), Int)
 compileExpression (AstCall (AstSymbol "if":b)) i = ifToInstructions 1 (AstCall (AstSymbol "if":b)) i
 compileExpression a i = case astToInstructions a i of
     (Right err, _) -> (Right err, i)
-    (Left il, index) -> (Left (il ++ [Instruction {line = index + 1, command = "return", value = Nothing}]), index + 1)
+    (Left il, index) -> (Left (il ++ [Instruction {line = index, command = "return", value = Nothing}]), index + 1)
 
 compile :: [Ast] -> Int -> (Either [Instruction] String)
 compile [] _ = (Left [])
 compile (a:b) i = case compileExpression a i of
-    (Right b, _) -> (Right b)
+    (Right err, _) -> (Right err)
     (Left x, index) -> case compile b (index + 1) of
         (Left res) -> (Left (x ++ res))
         Right err -> Right err
--- test/CompileSpec.hs
+++ test/CompileSpec.hs
@@ -4,10 +4,12 @@ import Test.Hspec
 import Compile
 import Lib
 
+-- compile [] 0 `shouldBe` (Left [
+-- ])
+
 compileIfSpec :: Spec
 compileIfSpec = do
     describe "simple if" $ do
-        -- 0 1 2 3 4 5
         it "(if #t 1 0)" $ do
             compile [(AstCall [AstSymbol "if", AstBoolean "#t", AstInteger 1, AstInteger 0])] 0 `shouldBe` (Left [
                     Instruction {line = 0, command = "push", value = Just (AstBoolean "#t")},
@@ -17,9 +19,61 @@ compileIfSpec = do
                     Instruction {line = 4, command = "push", value = Just (AstInteger 0)},
                     Instruction {line = 5, command = "return", value = Nothing}
                     ])
+        it "(define x (if #t 1 0))" $ do
+            compile [(AstDefine (Left (AstSymbol "x")) (AstCall [AstSymbol "if", AstBoolean "#t", AstInteger 1, AstInteger 0]))] 0 `shouldBe` (Left [
+                    Instruction {line = 0, command = "push", value = Just (AstBoolean "#t")},
+                    Instruction {line = 1, command = "jumpIfFalse", value = Just (AstInteger 3)},
+                    Instruction {line = 2, command = "push", value = Just (AstInteger 1)},
+                    Instruction {line = 3, command = "push", value = Just (AstInteger 0)},
+                    Instruction {line = 4, command = "call", value = Just (AstDefine (Left (AstSymbol "x")) (AstCall [AstSymbol "if", AstBoolean "#t", AstInteger 1, AstInteger 0]))},
+                    Instruction {line = 5, command = "return", value = Nothing}
+                    ])
 
--- compileSimpleExpression = do
+
+compileSimpleExpressionSpec :: Spec
+compileSimpleExpressionSpec = do
+    describe "simple expression" $ do
+        it "(x 1 2)" $ do
+            compile [AstCall [AstSymbol "x", AstInteger 1, AstInteger 2]] 0 `shouldBe` (Left [
+                Instruction {line = 0, command = "push", value = Just (AstInteger 1)},
+                Instruction {line = 1, command = "push", value = Just (AstInteger 2)},
+                Instruction {line = 2, command = "call", value = Just (AstSymbol "x")},
+                Instruction {line = 3, command = "return", value = Nothing}
+                ])
+        it "(x 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)" $ do
+            compile [AstCall [AstSymbol "x", AstInteger 1, AstInteger 2, AstInteger 3, AstInteger 4, AstInteger 5, AstInteger 6, AstInteger 7, AstInteger 8, AstInteger 9, AstInteger 10, AstInteger 11, AstInteger 12, AstInteger 13, AstInteger 14, AstInteger 15, AstInteger 16]] 0 `shouldBe` (Left [
+                Instruction {line = 0, command = "push", value = Just (AstInteger 1)},
+                Instruction {line = 1, command = "push", value = Just (AstInteger 2)},
+                Instruction {line = 2, command = "push", value = Just (AstInteger 3)},
+                Instruction {line = 3, command = "push", value = Just (AstInteger 4)},
+                Instruction {line = 4, command = "push", value = Just (AstInteger 5)},
+                Instruction {line = 5, command = "push", value = Just (AstInteger 6)},
+                Instruction {line = 6, command = "push", value = Just (AstInteger 7)},
+                Instruction {line = 7, command = "push", value = Just (AstInteger 8)},
+                Instruction {line = 8, command = "push", value = Just (AstInteger 9)},
+                Instruction {line = 9, command = "push", value = Just (AstInteger 10)},
+                Instruction {line = 10, command = "push", value = Just (AstInteger 11)},
+                Instruction {line = 11, command = "push", value = Just (AstInteger 12)},
+                Instruction {line = 12, command = "push", value = Just (AstInteger 13)},
+                Instruction {line = 13, command = "push", value = Just (AstInteger 14)},
+                Instruction {line = 14, command = "push", value = Just (AstInteger 15)},
+                Instruction {line = 15, command = "push", value = Just (AstInteger 16)},
+                Instruction {line = 16, command = "call", value = Just (AstSymbol "x")},
+                Instruction {line = 17, command = "return", value = Nothing}
+                ])
+        it "(lambda (x) 2)" $ do
+            compile [AstLambda [AstSymbol "x"] (AstInteger 2)] 0 `shouldBe` (Left [
+                Instruction {line = 0, command = "push", value = Just (AstLambda [AstSymbol "x"] (AstInteger 2))},
+                Instruction {line = 1, command = "return", value = Nothing}
+                ])
+        it "(define x 5)" $ do
+            compile [AstDefine (Left (AstSymbol "x")) (AstInteger 5)] 0 `shouldBe` (Left [
+                Instruction {line = 0, command = "push", value = Just (AstInteger 5)},
+                Instruction {line = 1, command = "call", value = Just (AstDefine (Left (AstSymbol "x")) (AstInteger 5))},
+                Instruction {line = 2, command = "return", value = Nothing}
+                ])
 
 spec :: Spec
 spec = do
     compileIfSpec
+    compileSimpleExpressionSpec
